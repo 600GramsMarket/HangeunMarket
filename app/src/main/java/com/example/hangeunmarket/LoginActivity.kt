2e8c925b85b14a1752ff67150455f6fd740c38d5
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-10-25 23:13:07 +0900

    메인 엑티비티 화면 전환 구현

--- app/src/main/java/com/example/hangeunmarket/LoginActivity.kt
+++ app/src/main/java/com/example/hangeunmarket/LoginActivity.kt
@@ -1,12 +1,28 @@
 package com.example.hangeunmarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.hangeunmarket.databinding.ActivityLoginBinding
+import com.example.hangeunmarket.databinding.ActivityMainBinding
 
 //로그인을 수행할 엑티비티 => 로그인 정보가 이미 앱에 있다면 자동 로그인?(나중에 구현할 것)
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        binding.btnSignUp.setOnClickListener {
+            //0. Intent 생성(요청 생성)
+            //1. 현재 앱의 Context와 이동할 엑티비티의 클래스
+            //의도 : LoginActivity에서 MainActivity로 화면 전환
+            val intent = Intent(this@LoginActivity,MainActivity::class.java)
+            startActivity(intent) //시스템에 요청 전송
+        }
     }
 }
