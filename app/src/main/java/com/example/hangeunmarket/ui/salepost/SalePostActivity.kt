1d0a6b770079c7eda94f76851c985defadebd57d
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-10-30 21:15:10 +0900

    판매글 보기 창 구현

---
+++ app/src/main/java/com/example/hangeunmarket/ui/salepost/SalePostActivity.kt
@@ -0,0 +1,14 @@
+package com.example.hangeunmarket.ui.salepost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hangeunmarket.R
+
+
+//판매 글 보기와 관련된 로직 처리
+class SalePostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sale_post)
+    }
+}
--- app/src/main/java/com/example/hangeunmarket/ui/salepost/SaleWritingActivity.kt
+++ app/src/main/java/com/example/hangeunmarket/ui/salepost/SaleWritingActivity.kt
@@ -1,7 +1,9 @@
 package com.example.hangeunmarket.ui.salepost
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.hangeunmarket.R
 import com.example.hangeunmarket.databinding.ActivitySaleWritingBinding
 
@@ -21,6 +23,16 @@ class SaleWritingActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_sale_writing)
 
+        var btnWriteDone = findViewById<Button>(R.id.btn_write_done)
+
         //글 쓰기 완료시
+        btnWriteDone.setOnClickListener{
+            val intent = Intent(this@SaleWritingActivity,SalePostActivity::class.java)
+
+            //번들에 데이터 넣어줘야함
+
+            startActivity(intent) //작성한 글 확인을 위해 넘어가기
+            finish() //현재 엑티비티는 종료처리
+        }
     }
 }
