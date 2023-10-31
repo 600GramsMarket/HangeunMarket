5cc600348f343118caccb65dd1a5144956ef79c4
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-10-31 20:22:22 +0900

    글쓰기 기능 부분 값 연결 구현

--- app/src/main/java/com/example/hangeunmarket/ui/salepost/SalePostActivity.kt
+++ app/src/main/java/com/example/hangeunmarket/ui/salepost/SalePostActivity.kt
@@ -2,13 +2,55 @@ package com.example.hangeunmarket.ui.salepost
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import com.example.hangeunmarket.R
 
 
 //판매 글 보기와 관련된 로직 처리
 class SalePostActivity : AppCompatActivity() {
+
+/*tv_sale_title
+* tv_sale_place
+* tv_sale_item_info
+* tv_sale_price //판매 가격
+* */
+
+    private lateinit var tvSaleTitle : TextView
+    private lateinit var tvSalePlace : TextView
+    private lateinit var tvSaleItemInfo : TextView
+    private lateinit var tvSalePrice : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sale_post)
+
+        tvSaleTitle = findViewById(R.id.tv_sale_title)
+        tvSalePlace = findViewById(R.id.tv_sale_place)
+        tvSaleItemInfo = findViewById(R.id.tv_sale_item_info)
+        tvSalePrice = findViewById(R.id.tv_sale_price)
+
+
+        /*
+        *   intent.putExtra("saleTitle",saleTitle.text)
+            intent.putExtra("salePrice",salePrice.text)
+            intent.putExtra("saleItemInfo",saleItemInfo.text)
+            intent.putExtra("salePlace",salePlace.text)
+        * */
+
+        var saleTitle = intent.getStringExtra("saleTitle")
+        var salePrice = intent.getStringExtra("salePrice")
+        var saleItemInfo = intent.getStringExtra("saleItemInfo")
+        var salePlace = intent.getStringExtra("salePlace")
+
+        Log.d("get data : ","${saleTitle},${salePrice},${saleItemInfo},${salePlace}")
+
+        //화면에 정보 뿌리기
+        tvSaleTitle.text = saleTitle
+        tvSalePlace.text = salePlace
+        tvSaleItemInfo.text = saleItemInfo
+        tvSalePrice.text = salePrice
+
     }
 }
--- app/src/main/java/com/example/hangeunmarket/ui/salepost/SaleWritingActivity.kt
+++ app/src/main/java/com/example/hangeunmarket/ui/salepost/SaleWritingActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import com.example.hangeunmarket.R
 import com.example.hangeunmarket.databinding.ActivitySaleWritingBinding
 
@@ -17,19 +18,33 @@ class SaleWritingActivity : AppCompatActivity() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var saleTitle : EditText //판매 제목
+    private lateinit var salePrice : EditText //판메 가격
+    private lateinit var salePlace : EditText //판매 장소
+    private lateinit var saleItemInfo : EditText //상품 설명
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_sale_writing)
 
-        var btnWriteDone = findViewById<Button>(R.id.btn_write_done)
+        saleTitle = findViewById<EditText>(R.id.et_set_title) //판매 글 제목
+        salePrice = findViewById<EditText>(R.id.et_set_price) //판매 가격
+        saleItemInfo = findViewById<EditText>(R.id.et_set_explain) //상품 설명
+        salePlace = findViewById<EditText>(R.id.et_set_place) //판매 희망 장소
 
+
+        var btnWriteDone = findViewById<Button>(R.id.btn_write_done)
         //글 쓰기 완료시
         btnWriteDone.setOnClickListener{
             val intent = Intent(this@SaleWritingActivity,SalePostActivity::class.java)
 
-            //번들에 데이터 넣어줘야함
+            //intent에 데이터 삽입 => 판매글 엑티비티로 넘기기
+            intent.putExtra("saleTitle",saleTitle.text.toString())
+            intent.putExtra("salePrice",salePrice.text.toString())
+            intent.putExtra("saleItemInfo",saleItemInfo.text.toString())
+            intent.putExtra("salePlace",salePlace.text.toString())
 
             startActivity(intent) //작성한 글 확인을 위해 넘어가기
             finish() //현재 엑티비티는 종료처리
