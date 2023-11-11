f8c0a680be1137ea2e1f08d779e543d6405b0779
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-11 17:44:37 +0900

    Add ChattingRoom Activity

---
+++ app/src/main/java/com/example/hangeunmarket/ui/chat/ChattingRoomActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hangeunmarket.ui.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hangeunmarket.R
+
+class ChattingRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatting_room)
+    }
+}
