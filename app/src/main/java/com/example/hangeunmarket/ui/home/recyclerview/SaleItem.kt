ce07218503ef32d4c85d49e669765c6e4ea72d63
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-13 17:07:44 +0900

    SalePostActivity Layout update

--- app/src/main/java/com/example/hangeunmarket/ui/home/recyclerview/SaleItem.kt
+++ app/src/main/java/com/example/hangeunmarket/ui/home/recyclerview/SaleItem.kt
@@ -3,6 +3,7 @@ package com.example.hangeunmarket.ui.home.recyclerview
 
 // 판매 상품 아이템 DTO
 data class SaleItem(
+    val id:String="", //판매 상품에 대한 아이디
     val saleItemImage: String = "",
     val saleTitle: String = "", // txt_sale_title 판매 제목
     val salePlace: String = "", // txt_sale_place 판매 장소
